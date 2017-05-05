381eac8e8e60d9de08b73fa31a5deeec5900bb20
Louis Racicot <info@louisracicot.com>
2017-05-05 00:58:14 -0400

    change firewall name in tests

--- Tests/Controller/UserControllerTest.php
+++ Tests/Controller/UserControllerTest.php
@@ -4,7 +4,6 @@ namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\BrowserKit\Cookie;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class UserControllerTest extends WebTestCase
@@ -66,7 +65,7 @@ class UserControllerTest extends WebTestCase
         $session = $this->client->getContainer()->get('session');
 
         // the firewall context defaults to the firewall name
-        $firewallContext = 'secured_area';
+        $firewallContext = 'main';
 
         $token = new UsernamePasswordToken('admin', null, $firewallContext, array('ROLE_ADMIN'));
         $session->set('_security_'.$firewallContext, serialize($token));
